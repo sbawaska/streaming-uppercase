24652254de9c30b6ed685bd0698be5f899c29c39
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-10-21 17:04:38 -0700

    initial commit

---
+++ src/main/java/io/projectriffdemo/streaminguppercase/StreaminguppercaseApplication.java
@@ -0,0 +1,22 @@
+package io.projectriffdemo.streaminguppercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class StreaminguppercaseApplication {
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> streamingUppercase() {
+        return stringFlux -> stringFlux.map(String::toUpperCase);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreaminguppercaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/projectriffdemo/streaminguppercase/StreaminguppercaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projectriffdemo.streaminguppercase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreaminguppercaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
